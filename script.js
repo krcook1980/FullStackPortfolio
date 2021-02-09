cff367439053b8499def1912298505e8880a6384
Kelly Cook <kellyrcook1980@gmail.com>
2021-02-08 23:59:00 -0700

    downloadable resume no longer link to google drive, email working though I don't like it

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
- 
-    var emailAddress = ($(".email").val());
-    var comment = $(".comment").val();
-    var sender = $(".name").val();
+//form html and js from formspree.io
 
+  window.addEventListener("DOMContentLoaded", function() {
 
-$(".submit").click(function(){
+    // get the form elements defined in your form HTML above
     
-    Email.send({
-        SecureToken : "cdddc7fe-dd97-41c0-88ab-69112add13e4",
-        To : "kellyrcook1980@gmail.com",
-        From : "kellyrcook1980@gmail.com",
-        Subject : "Portfolio Contact Request",
-        Body : "Hi " + comment + " from " + sender + " at " + emailAddress
-    }).then(function (message) {
-        $(".emailNotification").empty();
-        
-        if (message=="OK"){
-            $(".emailNotification").append("<p>"+"Thank you, I will get back to you soon!"+"</p>");
-        }
-        else {
-            $(".emailNotification").append("<p>"+"I'm sorry, something went wrong, please try again."+"</p>")
-        }
-        console.log(message);
-    })
-});
+    var form = $("#my-form");
+    var button = $("#my-form-button");
+    var status = $("#my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thank you for reaching out to me!";
+    }
+
+    function error() {
+      status.innerHTML = "I'm sorry, something went wrong. Please try again.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    // xhr.open(method, url);
+    // xhr.setRequestHeader("Accept", "application/json");
+    // xhr.onreadystatechange = function() {
+    //   if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    //   if (xhr.status === 200) {
+    //     success(xhr.response, xhr.responseType);
+    //   } else {
+    //     error(xhr.status, xhr.response, xhr.responseType);
+    //   }
+    };
+    xhr.send(data);
+  }
+
+
